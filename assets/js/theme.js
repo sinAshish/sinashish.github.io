7372a96cb61cf50330ee0d3f032c154ba7ce664b
Ashish Sinha <ashishsinha108@gmail.com>
2026-06-19 14:24:28 -0700

    Fix: completely remove commented-out theme toggle event listener

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -282,17 +282,6 @@ let initTheme = () => {
 
   setThemeSetting(themeSetting);
 
-  // Add event listener to the theme toggle button.
-  /*
-  document.addEventListener("DOMContentLoaded", function () {
-    const mode_toggle = document.getElementById("light-toggle");
-
-    mode_toggle.addEventListener("click", function () {
-      toggleThemeSetting();
-    });
-  });
-  */
-
   // Add event listener to the system theme preference change.
   window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", ({ matches }) => {
     applyTheme();
