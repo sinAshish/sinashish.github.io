0f819426db67c76727b4fe8b79f10f9c4f5a9070
sinAshish <ashishsinha108@gmail.com>
2026-09-07 00:47:58 +0000

    Deploying to gh-pages from @ sinAshish/sinashish.github.io@4b27dda608f5d2df2d8bbddc1c86c73eedc4c3f2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
+  },{id: "nav-reading-list",
+          title: "reading list",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/reading/";
+          },
+        },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
