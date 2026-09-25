a72d0b3edc0ce015e547765c2c533ad63b52b88f
sinAshish <ashishsinha108@gmail.com>
2026-09-25 00:49:32 +0000

    Deploying to gh-pages from @ sinAshish/sinashish.github.io@55202a8acd6893c2c7ca68c3090e9d5c34758fe3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "My academic CV.",
+          description: "My academic CV",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
